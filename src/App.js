114580f594ffdcc0afa1530f438b13b4003159b9
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2021-09-11 13:44:18 -0500

    get user adress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import { useEffect } from "react";
+import Geocode from "react-geocode";
+Geocode.setApiKey(process.env.REACT_APP_GEO_KEY);
 
 function App() {
   useEffect(() => {
-    console.log(process.env.REACT_APP_GEO_KEY);
-  });
+    if (!navigator.geolocation) {
+      console.log("geo not allowed");
+    } else {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(latitude, longitude);
+        Geocode.fromLatLng(latitude, longitude).then(
+          (response) => {
+            const address = response.results[0].formatted_address;
+            console.log(address);
+          },
+          (error) => console.log(error)
+        );
+      });
+    }
+  }, []);
   return <div className="App">multi-step form</div>;
 }
 
