b69eb9f56316f4c9c67b3077c5b7d73130ff2267
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2021-09-11 14:35:41 -0500

    setCity

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ const key = process.env.REACT_APP_GEO_KEY;
 const reverseGeoUrl = "https://us1.locationiq.com/v1/reverse.php";
 function App() {
   const [LocationObject, setLocationObject] = useState();
+  const [city, setCity] = useState("");
+
+  const handleChange = (e) => {
+    const { value } = e.currentTarget;
+    setCity(value);
+  };
+
   useEffect(() => {
     if (!navigator.geolocation) {
       console.log("geo not allowed");
@@ -13,15 +20,31 @@ function App() {
         const { longitude } = position.coords;
         const url = `${reverseGeoUrl}?key=${key}&lat=${latitude}&lon=${longitude}&format=json`;
         axios.get(url).then((res) => {
-          // console.log(res);
           const { data } = res;
+          const { city } = data.address;
           setLocationObject(data);
+          setCity(city);
         });
       });
     }
   }, []);
-  console.log(LocationObject, " location object");
-  return <div className="App">multi-step form</div>;
+  // console.log("location object= ", LocationObject);
+  return (
+    <div className="App">
+      <form>
+        <label>
+          Policy Location
+          <input
+            type="text"
+            name="policy location"
+            value={city}
+            onChange={handleChange}
+          />
+        </label>
+        {/* <input type="submit" value="Submit" /> */}
+      </form>
+    </div>
+  );
 }
 
 export default App;
